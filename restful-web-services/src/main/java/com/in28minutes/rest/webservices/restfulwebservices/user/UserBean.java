f7ee3f30df9eccadc259a02c7de37b9536c538b5
abc <abc@example.com>
2020-08-30 22:30:52 +0530

    Web Service

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserBean.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+public class UserBean {
+
+	private String message;
+	
+	
+
+	public UserBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user values(100001,sysdate(),'Ab');
+insert into user values(100002,sysdate(),'jill');
+insert into user values(100003,sysdate(),'jack');
+insert into post values(11001,'My first post',100001);
+insert into post values(11002,'My Second post',100001);
+insert into post values(11101,'First Post',100002);
+insert into post values(11102,'Second post',100002);
